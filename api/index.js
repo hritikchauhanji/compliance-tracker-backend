f14e6d2651b486efede03d186e9ab7ae82879b59
hritikchauhanji <hritikchji@gmail.com>
2026-03-23 04:57:55 +0530

    fix: update import statement to include file extension and adjust root directory in tsconfig

---
+++ api/index.js
@@ -0,0 +1,5 @@
+import { app } from "../src/app.js";
+export default async function handler(req, res) {
+    await app.ready();
+    app.server.emit("request", req, res);
+}
--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { app } from "../src/app";
+import { app } from "../src/app.js";
 
 export default async function handler(req: any, res: any) {
   await app.ready();
